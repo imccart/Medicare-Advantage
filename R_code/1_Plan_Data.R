a089fb8b667a27f3795bbf8bd203d2bbcf6641b8
Ian McCarthy <ianmccarthy.econ@gmail.com>
2019-07-09 14:48:35 -0400

    Adding R versions of plan data

---
+++ R_code/1_Plan_Data.R
@@ -0,0 +1,102 @@
+#########################################################################
+## Read in enrollment data
+#########################################################################
+## Set local "month lists" to identify different files relevant for each year
+## Month lists differ by year just in case you work with data that are only available
+## in a fraction of a year, which often happens for new data as new monthly releases
+## are made. Some data sources are also only available in certain years.
+#########################################################################
+
+monthlist_2006=c("07", "08", "09", "10", "11", "12")
+monthlist_2007=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2008=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2009=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2010=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2011=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2012=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2013=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2014=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+monthlist_2015=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+
+for (y in 2006:2015) {
+  monthlist=get(paste("monthlist_",y,sep=""))
+  for (m in monthlist) {
+    
+    ## Basic contract/plan information
+    ma.path=paste(path.data.ma,"\\Monthly MA and PDP Enrollment by CPSC\\Monthly MA and PDP Enrollment by CPSC\\Extracted Data\\CPSC_Contract_Info_",y,"_",m,".csv",sep="")
+    contract.info=read.csv(ma.path,col.names=c("contractid","planid","org_type","plan_type","partd","snp","eghp","org_name","org_marketing_name","plan_name","parent_org","contract_date"),stringsAsFactors=FALSE)
+
+    contract.info = contract.info %>%
+      group_by(contractid, planid) %>%
+      mutate(id_count=row_number())
+    
+    contract.info = contract.info %>%
+      filter(id_count==1) %>%
+      select(-id_count)
+    
+    ## Enrollments per plan
+    ma.path=paste(path.data.ma,"\\Monthly MA and PDP Enrollment by CPSC\\Monthly MA and PDP Enrollment by CPSC\\Extracted Data\\CPSC_Enrollment_Info_",y,"_",m,".csv",sep="")    
+    enroll.info=read.csv(ma.path,col.names=c("contractid","planid","ssa","fips","state","county","enrollment"),stringsAsFactors=FALSE)
+    
+    enroll.info = enroll.info %>%
+      mutate(enrollment=replace(enrollment,enrollment=="*",NA)) %>%
+      as.data.frame()
+    
+    enroll.info = enroll.info %>%
+      mutate(enrollment=as.numeric(enrollment))
+
+    ## Merge contract info with enrollment info
+    plan.data = contract.info %>%
+      left_join(enroll.info, by=c("contractid", "planid")) %>%
+      mutate(month=as.numeric(m),year=y)
+    
+    assign(paste("plan.data.",y,".",m,sep=""),plan.data)
+  }
+  
+  ## Append monthly enrollment info for each year
+  step=0
+  for (m in monthlist) {
+    step=step+1
+    assign(paste("plan.month.",step,sep=""),get(paste("plan.data.",y,".",m,sep="")))
+    if (step==1) {
+      plan.month=plan.month.1
+    } else {
+      plan.month=rbind(plan.month,get(paste("plan.month.",step,sep="")))
+    }
+  }
+  
+  ## Fill in missing fips codes (by state and county)
+  plan.month = plan.month %>%
+    group_by(state, county) %>%
+    fill(fips)
+
+  ## Fill in missing plan characteristics by contract and plan id
+  plan.month = plan.month %>%
+    group_by(contractid, planid) %>%
+    fill(plan_type, partd, snp, eghp, plan_name)
+  
+  ## Fill in missing contract characteristics by contractid
+  plan.month = plan.month %>%
+    group_by(contractid) %>%
+    fill(org_type,org_name,org_marketing_name,parent_org)
+    
+  ## Collapse from monthly data to yearly
+  plan.year = plan.month %>%
+    group_by(contractid, planid, fips) %>%
+    arrange(contractid,planid,fips,month) %>%
+    summarize(avg_enrollment=mean(enrollment),sd_enrollment=sd(enrollment),
+              min_enrollment=min(enrollment),max_enrollment=max(enrollment),
+              first_enrollment=first(enrollment),last_enrollment=last(enrollment),
+              state=last(state),county=last(county),org_type=last(org_type),
+              plan_type=last(plan_type),partd=last(partd),snp=last(snp),
+              eghp=last(eghp),org_name=last(org_name),org_marketing_name=last(org_marketing_name),
+              plan_name=last(plan_name),parent_org=last(parent_org),contract_date=last(contract_date),
+              year=last(year))
+    
+  assign(paste("plan.year.",y,sep=""),plan.year)
+}
+
+full.ma.data=rbind(plan.year.2006,plan.year.2007,plan.year.2008,plan.year.2009,plan.year.2010,
+                   plan.year.2011,plan.year.2012,plan.year.2013,plan.year.2014,plan.year.2015)
+
+write_tsv(full.ma.data,path=paste(path.data.final,"\\Full_Contract_Plan_County.txt",sep=""),append=FALSE,col_names=TRUE)
---
+++ R_code/_MA_Data_Internal.R
@@ -0,0 +1,103 @@
+########################################################################################
+## Author:        Ian McCarthy
+## Date Created:  7/8/2019
+## Date Edited:   7/8/2019
+## Notes:         R file to build Medicare Advantage dataset
+########################################################################################
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate)
+
+#########################################################################
+## First set all file/folder paths
+#########################################################################
+path.code="D:\\CloudStation\\Professional\\Research Projects\\_Git\\medicare-advantage\\R_code"
+path.data.final="D:\\CloudStation\\Professional\\Research Projects\\_Git\\medicare-advantage\\data"
+path.data.ma="D:\\CloudStation\\Professional\\Research Data\\Medicare Advantage"
+path.data.ffs="D:\\CloudStation\\Professional\\Research Data\\Medicare FFS"
+
+
+#########################################################################
+## Build plan-level dataset
+#########################################################################
+source(paste(path.code,"\\1_Plan_data.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\2_Plan_Characteristics.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\3_Serivce_Areas.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\4_Penetration_Files.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\5_Star_Ratings.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\6_Risk_Rebates.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\7_MA_Benchmark.R",sep=""),local=TRUE,echo=FALSE)
+source(paste(path.code,"\\8_FFS_Costs.R",sep=""),local=TRUE,echo=FALSE)
+
+
+#########################################################################
+## Organize final data
+#########################################################################
+
+use "${DATA_FINAL}Full_Contract_Plan_County.dta", clear
+merge m:1 contractid fips year using "${DATA_FINAL}Contract_Service_Area.dta", keep(master match)
+
+** Part C Approved Plans
+gen CMS_Approved=(_merge==3)
+drop _merge
+
+** Drop special needs plans and other specific observations
+drop if state=="VI" | state=="PR" | state=="MP" | state=="GU" | state=="AS"
+drop if snpplan=="Yes"
+drop if planid>=800 & planid<=899
+drop if planid==.
+drop if fips==.
+keep if CMS_Approved==1
+drop CMS_Approved
+
+** Merge remaining files
+merge m:1 contractid year using "${DATA_FINAL}Star_Ratings.dta", keep(master match) nogenerate
+merge m:1 fips year using "${DATA_FINAL}MA_Penetration.dta", keep(master match) nogenerate
+
+gen byte nonmiss=!mi(statename)
+sort state statename nonmiss
+bys state (nonmiss): replace statename=statename[_N] if nonmiss==0
+drop nonmiss
+rename state state_short
+rename statename state
+merge 1:1 contractid planid state county year using "${DATA_FINAL}Plan_Premiums.dta", keep(master match) nogenerate
+merge m:1 contractid planid year using "${DATA_FINAL}RiskRebate.dta", keep(master match) nogenerate
+merge m:1 ssa year using "${DATA_FINAL}MA_Benchmark.dta", keep(master match) nogenerate
+
+
+*****************************************************  
+** Calculate star rating (part C/D rating if plan offers Part D, otherwise Part C rating only)
+gen StarRating=PartC_Summary_Star if offerspartd=="No" | (offerspartd=="Yes" & PartCD_Summary_Star==.)
+replace StarRating=PartCD_Summary_Star if offerspartd=="Yes" & PartCD_Summary_Star!=.
+qui tab StarRating, gen(starfx)
+
+*****************************************************  
+** Benchmarks
+gen ma_rate=Risk if year<2012
+replace ma_rate=Risk_Star50 if StarRating==5   & year>=2012 & year<2015
+replace ma_rate=Risk_Star45 if StarRating==4.5 & year>=2012 & year<2015
+replace ma_rate=Risk_Star40 if StarRating==4   & year>=2012 & year<2015
+replace ma_rate=Risk_Star35 if StarRating==3.5 & year>=2012 & year<2015
+replace ma_rate=Risk_Star30 if StarRating==3   & year>=2012 & year<2015
+replace ma_rate=Risk_Star25 if StarRating<3    & year>=2012 & year<2015
+replace ma_rate=Risk_Star35 if StarRating==.   & year>=2012 & year<2015
+replace ma_rate=Risk_Bonus5 if StarRating>=4   & year==2015
+replace ma_rate=Risk_Bonus0 if StarRating<4    & year==2015
+replace ma_rate=Risk_Bonus35 if StarRating==.  & year==2015
+
+*****************************************************  
+** Premium and bid variables
+gen PartD=(offerspartd=="Yes")
+gen basic_premium=Premium_PartC
+replace basic_premium=0 if Rebate_PartC>0 & Rebate_PartC!=.
+replace basic_premium=Premium if PartD==0 & Premium!=. & basic_premium==.
+gen bid=Payment_PartC/RiskScore_PartC if (Rebate_PartC>0 | basic_premium==0)
+replace bid=(Payment_PartC+basic_premium)/RiskScore_PartC if (Rebate_PartC==0 & basic_premium>0 & basic_premium!=.)
+
+*****************************************************  
+/* Incorporate Medicare FFS Cost Data */
+merge m:1 ssa year using "${DATA_FINAL}FFS_Costs.dta", keep(master match) nogenerate
+gen AvgFFSCost=(parta_reimb/parta_enroll) + (partb_reimb/partb_enroll)
+
+save "${DATA_FINAL}MA_Data.dta", replace
+
+
